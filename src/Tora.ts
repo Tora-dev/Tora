5962e28436bad953f2e09b05a7946bb30180c10e
eyhn <cneyhn@gmail.com>
2017-05-03 13:18:22 +0800

    test(logger): test for logger

---
+++ src/Tora.ts
@@ -0,0 +1,5 @@
+import { EventEmitter } from 'events';
+
+class Tora extends EventEmitter {
+
+}
---
+++ test/utils/Logger.test.ts
@@ -0,0 +1,18 @@
+import { expect, sandbox } from '../helper';
+import Logger from '../../src/utils/Logger';
+
+describe('Logger', () => {
+  it('Logs unhandled promises errors', (finish) => {
+    const errorFun = sandbox.stub(Logger, 'error');
+    const testerror = new Error('error');
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(testerror);
+      }, 50);
+    });
+    setTimeout(() => {
+      expect(errorFun).to.be.calledOnce.and.calledWith(testerror);
+      finish();
+    }, 100);
+  });
+});
